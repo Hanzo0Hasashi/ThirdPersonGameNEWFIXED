51c35e53ca234eaa7b0bb1052982c1820b59ce01
Hanzo0Hasashi <167780602+Hanzo0Hasashi@users.noreply.github.com>
2026-04-01 17:54:13 +0300

    Add EntryPoint scene with MainMenu UI, GameLevel scene, and player movement with jump

---
+++ ThirdPersonGameNEWFIXED/Assets/GameManager.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Cursor.lockState = CursorLockMode.None;
+            SceneManager.LoadScene("EntryPoint");
+        }
+    }
+}
---
+++ ThirdPersonGameNEWFIXED/Assets/MainMenu.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public void StartGame()
+    {
+        SceneManager.LoadScene("GameScene");
+    }
+
+    public void QuitGame()
+    {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+            Application.Quit();
+#endif
+    }
+}
